c51b905a3b79359360e08a0de4b7f6943c0c53b0
QuaCN-TripOTA <qua.cn@tripota.com.vn>
2025-09-20 17:28:52 +0700

    Update arch-quacn.sh

--- arch-quacn.sh
+++ arch-quacn.sh
@@ -1,63 +1,56 @@
 #!/bin/bash
 set -e
 
-# Thiết lập timezone
-echo "==> Thiết lập timezone"
+echo "==> Setup timezone"
 ln -sf /usr/share/zoneinfo/Asia/Ho_Chi_Minh /etc/localtime
 timedatectl set-timezone Asia/Ho_Chi_Minh
 timedatectl set-ntp true
 
-# Cấu hình locale
-echo "==> Cấu hình locale"
+echo "==> Configure locale"
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
-# Hostname
 echo "==> Hostname"
-echo "tripota" > /etc/hostname
-if ! grep -q "127.0.0.1 tripota.localdomain tripota" /etc/hosts; then
-    echo "127.0.0.1 tripota.localdomain tripota" >> /etc/hosts
+echo "seiza" > /etc/hostname
+if ! grep -q "127.0.0.1 seiza.localdomain seiza" /etc/hosts; then
+    echo "127.0.0.1 seiza.localdomain seiza" >> /etc/hosts
 fi
 
-# Thiết lập mật khẩu root
-echo "==> Đặt mật khẩu root"
+echo "==> Setup password for root"
 passwd
 
-# Tạo user mới
-useradd -mG wheel quacn
-echo "==> Đặt mật khẩu cho user: quacn"
-passwd quacn
+echo "==> Create and setup password for user: licons"
+useradd -mG wheel licons
+passwd licons
 
-# Sudoers
-echo "==> Mở file sudoers để bỏ comment các dòng liên quan đến wheel"
+echo "==> Setup wheel"
 EDITOR=nano visudo
 
-# Pacman multilib
-echo "==> Mở /etc/pacman.conf để bỏ comment [multilib] và Include"
+echo "==> Open /etc/pacman.conf uncomment [multilib] và Include"
 nano /etc/pacman.conf
 
-# Update pacman
+echo "==> Update pacman"
 pacman -Syu --noconfirm pacman
 
-# mkinitcpio chỉnh preset
-echo "==> Mở /etc/mkinitcpio.d/linux.preset và chỉnh PRESETS=('default'), comment fallback"
+echo "==> Open /etc/mkinitcpio.d/linux.preset"
+echo "===> Update PRESETS=('default')"
+echo "===> #fallback"
 nano /etc/mkinitcpio.d/linux.preset
 rm -f /boot/initramfs-linux-fallback.img
 mkinitcpio -P
 
-# Cài grub EFI
+echo "==> GRUB Install"
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 
-# Cài theme cho grub
+echo "==> Install theme for GRUB"
 cd /tmp
 git clone https://github.com/vinceliuice/grub2-themes.git
 cd grub2-themes
 ./install.sh -t tela
 cd /
 
-# Cấu hình grub
-echo "==> Mở /etc/default/grub và chỉnh lại:"
+echo "==> Config GRUB"
 echo "GRUB_DEFAULT=saved"
 echo "GRUB_TIMEOUT=2"
 echo "GRUB_DISABLE_OS_PROBER=false"
@@ -68,10 +61,10 @@ nano /etc/default/grub
 chmod -x /etc/grub.d/30_uefi-firmware
 grub-mkconfig -o /boot/grub/grub.cfg
 
-echo "==> Hoàn tất cấu hình cơ bản!"
+echo "==> Install ArchLinux completed!"
 
 
-echo "==> Cài driver NVIDIA"
+echo "==> Install NVIDIA driver"
 pacman -S --noconfirm \
     nvidia-dkms nvidia-utils nvidia-settings \
     lib32-nvidia-utils lib32-opencl-nvidia
@@ -80,12 +73,12 @@ echo "options nvidia-drm modeset=1" > /etc/modprobe.d/nvidia.conf
 echo "blacklist nouveau" > /etc/modprobe.d/blacklist-nouveau.conf
 
 ### ==== KDE ====
-echo "==> Cài KDE và các apps"
+echo "==> Install KDE and Apps"
 pacman -S --noconfirm \
     plasma-meta \
     sddm sddm-kcm \
-    konsole dolphin dolphin-plugins spectacle ark gwenview kcalc okular \
-    plasma-discover packagekit-qt5 flatpak \
+    konsole dolphin dolphin-plugins spectacle ark gwenview kalk kate okular \
+    flatpak pacman-contrib \
     pipewire pipewire-audio pipewire-alsa pipewire-jack wireplumber \
     bluez bluez-utils bluedevil \
     powerdevil power-profiles-daemon \
@@ -106,4 +99,4 @@ systemctl enable fstrim.timer
 
 usermod -aG docker quacn
 
-echo "==> Hoàn tất cài đặt!"
+echo "==> Install KDE and Apps completed!"
